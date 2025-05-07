54f35c7a7b911d9a43887aeb620f34848a67f20a
Sebastian Sanjorge <sanjorgesebastian@gmail.com>
2025-05-07 02:37:11 -0300

    feat: expose CharacterDetail component via module federation

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "remote-app",
+      filename: "remoteEntry.js",
+      // Modules to expose
+      exposes: {
+        "./CharacterDetail": "./src/components/CharacterDetail.tsx",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      input: "./index.html",
+    },
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: true,
+    modulePreload: false,
+  },
+});
