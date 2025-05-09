4962e2764d4bea865e11566ba7290dc19b935b49
Sebastian Sanjorge <sanjorgesebastian@gmail.com>
2025-05-09 03:21:09 -0300

    fix: ultimo commit en vite - mudanza a webpack federation para resolver doble instancia de react-redux

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
