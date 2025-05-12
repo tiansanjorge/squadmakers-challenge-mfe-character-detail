b34502ebff16fcf7455b05a4b7cda8c2fff63249
Sebastian Sanjorge <sanjorgesebastian@gmail.com>
2025-05-12 04:08:47 -0300

    fix: modal crash

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
 import React from "react";
 import CharacterDetail from "./components/CharacterDetail";
+import "./index.css";
+import { MoreVertical, X } from "lucide-react";
 
 function App() {
   return (
     <>
-      <CharacterDetail
-        nombre="Rick Sanchez"
-        imagen="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-        especie="Humano"
-        ubicacion="Citadel of Ricks"
-        estado="Vivo"
-        genero="Masculino"
-        origen="Earth (C-137)"
-        episodios={[
-          { nombre: "Pilot", codigo: "S01E01" },
-          { nombre: "Lawnmower Dog", codigo: "S01E02" },
-        ]}
-        backgroundImageUrl="/detail-bg.jpg"
-      />
+      <div className="tw-fixed tw-inset-0 tw-z-50 tw-bg-black tw-bg-opacity-60 tw-flex tw-items-center tw-justify-center sm:tw-px-0">
+        <div className="tw-bg-white tw-rounded-xl tw-w-full sm:tw-max-w-lg tw-relative tw-shadow-lg tw-max-h-[95vh] tw-overflow-y-auto">
+          {/* Botón de cerrar y 3 puntitos visible solo en desktop */}
+          <button className="tw-hidden sm:tw-flex tw-bg-white tw-p-2 tw-rounded-full tw-absolute tw-top-4 tw-right-4 tw-text-gray-500 hover:tw-text-gray-700 tw-z-10">
+            <X className="tw-w-5 tw-h-5" />
+          </button>
+          <MoreVertical className="tw-absolute tw-top-[165px] tw-right-5 tw-w-6 tw-h-6 tw-text-[#808C73] tw-cursor-pointer" />
+          <CharacterDetail
+            nombre="Rick Sanchez"
+            imagen="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+            especie="Humano"
+            ubicacion="Citadel of Ricks"
+            estado="Vivo"
+            genero="Masculino"
+            origen="Earth (C-137)"
+            episodios={[
+              { nombre: "Pilot", codigo: "S01E01" },
+              { nombre: "Lawnmower Dog", codigo: "S01E02" },
+            ]}
+            backgroundImageUrl="/detail-bg.jpg"
+          />
+        </div>
+      </div>
     </>
   );
 }
--- src/components/CharacterDetail.tsx
+++ src/components/CharacterDetail.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import "../index.css";
 import starFull from "../assets/star-full.png";
 import starEmpty from "../assets/star-empty.png";
+import { ArrowLeft } from "lucide-react";
+import { MoreVertical } from "lucide-react";
 
 type CharacterDetailProps = {
   nombre: string;
@@ -18,6 +20,7 @@ type CharacterDetailProps = {
   esFavorito?: boolean;
   onToggleFavorito?: () => void;
   backgroundImageUrl?: string;
+  onBack?: () => void;
 };
 
 const CharacterDetail = ({
@@ -32,65 +35,93 @@ const CharacterDetail = ({
   esFavorito = false,
   onToggleFavorito,
   backgroundImageUrl,
+  onBack,
 }: CharacterDetailProps) => {
   const starSrc = esFavorito ? starFull : starEmpty;
   const estadoColor =
     estado === "Vivo"
-      ? "bg-green-100 text-green-700"
-      : "bg-gray-100 text-gray-700";
+      ? "tw-bg-green-100 tw-text-green-700"
+      : estado === "Muerto"
+      ? "tw-bg-red-100 tw-text-red-700"
+      : "tw-bg-gray-100 tw-text-gray-700";
 
   return (
-    <div className="max-w-3xl mx-auto rounded-xl overflow-hidden shadow-lg bg-gray-50">
-      {/* Header con imagen */}
+    <div className="tw-rounded-xl tw-bg-[#F0F1EC]">
+      {/* Header */}
       <div
-        className="min-h-32 bg-cover bg-center relative"
+        className="tw-relative tw-h-[128px] tw-bg-cover tw-bg-top 
+      before:tw-content-[''] before:tw-absolute before:tw-inset-0 
+      before:tw-bg-[linear-gradient(0deg,_rgba(0,0,0,0)_-10%,_rgba(0,0,0,0.8)_80%)] 
+      before:tw-bg-blend-multiply sm:before:tw-hidden 
+      sm:after:tw-content-[''] sm:after:tw-absolute sm:after:tw-inset-0 
+      sm:after:tw-bg-black sm:after:tw-opacity-40"
         style={{
           backgroundImage: backgroundImageUrl
             ? `url(${backgroundImageUrl})`
             : undefined,
         }}
       >
+        {/* Botones */}
+        {!onBack && (
+          <div>
+            <button
+              onClick={onBack}
+              className="tw-absolute tw-top-3 tw-left-3 tw-rounded-full tw-w-10 tw-h-10 tw-flex tw-items-center tw-justify-center tw-shadow sm:tw-hidden"
+            >
+              <ArrowLeft className="tw-w-8 tw-h-8 tw-text-white" />
+            </button>
+            <MoreVertical className="tw-absolute tw-top-4 tw-right-3 tw-w-8 tw-h-8 tw-text-white tw-cursor-pointer" />
+          </div>
+        )}
+
         <img
           src={imagen}
           alt={nombre}
-          className="w-32 h-32 rounded-full border-4 border-white absolute left-4 top-[64px]"
+          className="tw-w-24 tw-h-24 sm:tw-w-32 sm:tw-h-32 tw-rounded-full tw-border-4 tw-border-white tw-absolute tw-left-6 tw-top-[64px] tw-z-10"
         />
       </div>
 
-      {/* Contenido */}
-      <div className="pt-4 px-6 pb-6">
-        <div className="ml-36">
-          <div className="flex  gap-3 ">
-            <h2 className="text-2xl font-bold">{nombre}</h2>
-            <button onClick={onToggleFavorito} className="">
-              <img src={starSrc} alt="favorito" className="w-5 h-5" />
+      <div className="tw-pt-4 tw-px-5 tw-pb-5">
+        <div className="tw-text-center sm:tw-text-left sm:tw-ml-40">
+          <div className="tw-flex tw-items-center tw-justify-center sm:tw-justify-start tw-gap-3 tw-mb-1">
+            <h2 className="tw-text-2xl tw-font-bold">{nombre}</h2>
+            <button onClick={onToggleFavorito}>
+              <img src={starSrc} alt="favorito" className="tw-w-5 tw-h-5" />
             </button>
           </div>
-
-          <p className="text-gray-500">{especie}</p>
+          <p className="tw-text-gray-500 tw-text-sm">{especie}</p>
         </div>
-        <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 gap-6">
-          {/* Info */}
-          <div className="bg-white p-4 rounded-lg shadow">
-            <h3 className="font-semibold text-gray-700 mb-2">Información</h3>
-            <p className="text-sm text-gray-600">
-              <span className="font-semibold">Género:</span> {genero}
+
+        <div className="tw-mt-6 tw-flex tw-justify-between tw-gap-4">
+          <div className="tw-bg-white tw-p-4 tw-rounded-lg tw-shadow">
+            <h3 className="tw-font-semibold tw-text-gray-700 tw-mb-2">
+              Información
+            </h3>
+            <p className="tw-text-sm tw-text-gray-600">
+              <span className="tw-text-sm tw-font-semibold tw-text-[#808C73]">
+                Género:
+              </span>
+              <br /> {genero}
             </p>
-            <p className="text-sm text-gray-600">
-              <span className="font-semibold">Origen:</span> {origen}
+            <p className="tw-text-sm tw-text-gray-600">
+              <span className="tw-text-sm tw-font-semibold tw-text-[#808C73]">
+                Origen:
+              </span>
+              <br /> {origen}
             </p>
-            <p className="text-sm text-gray-600 mt-2">Estado:</p>
+            <p className="tw-text-sm tw-text-gray-600 tw-mt-2">Estado:</p>
             <span
-              className={`inline-block mt-1 px-3 py-1 text-sm font-medium rounded-full ${estadoColor}`}
+              className={`tw-inline-block tw-mt-1 tw-px-3 tw-py-1 tw-text-sm tw-font-medium tw-rounded-full ${estadoColor}`}
             >
               {estado}
             </span>
           </div>
 
-          {/* Episodios */}
-          <div className="bg-white p-4 rounded-lg shadow">
-            <h3 className="font-semibold text-gray-700 mb-2">Episodios</h3>
-            <ul className="text-sm text-gray-600 space-y-1">
+          <div className="tw-bg-white tw-p-4 tw-rounded-lg tw-shadow tw-w-full">
+            <h3 className="tw-font-semibold tw-text-gray-700 tw-mb-2">
+              Episodios
+            </h3>
+            <ul className="tw-text-sm tw-text-gray-600 tw-space-y-1">
               {episodios.slice(0, 5).map((ep, i) => (
                 <li key={i}>
                   {ep.codigo} - {ep.nombre}
